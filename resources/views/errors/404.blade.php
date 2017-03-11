d2066817a1488443a8a40d0b015ecd9257cdcdae
Cohan Robinson <cohan@icnerd.com>
2017-03-11 22:21:10 +0000

    Removed the shiiiit site as it's not https. Added Yoglabs vid for now

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,9 +7,29 @@
 			404 - That's a page not found
 		</article>
 
-		<div>
-		<iframe src='http://shiiiit.com/' style='width:100%;height:1500px;border:none'></iframe>
+		<style>
+			.embed-container {
+				position: relative;
+				padding-bottom: 56.25%;
+				height: 0;
+				overflow: hidden;
+				max-width: 100%;
+			}
+			.embed-container iframe,
+			.embed-container object,
+			.embed-container embed {
+				position: absolute; 
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+			}
+		</style>
+
+		<div class='embed-container col-sm-12 alert alert-danger'>
+			<iframe src='https://www.youtube.com/embed/qOVLUiha1B8' frameborder='0' allowfullscreen></iframe>
 		</div>
+
 	</div>
 </div> 
 @endsection
