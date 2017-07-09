985d94fafebe371373517239d82f37b824ec6968
Cohan Robinson <cohan@icnerd.com>
2017-07-09 04:56:11 +0100

    Add scheduled sitemap build

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,6 +51,8 @@ class Kernel extends ConsoleKernel
 
 		$schedule->command('backup:clean')->daily()->at('01:00');
 		$schedule->command('backup:run')->daily()->at('02:00');
+
+		$schedule->command('sitemap:generate')->daily()->at('06:00');
 	}
 
 	/**
