202a4a23fa568c2e2086fa2c69a0ea470733b3ae
Cohan Robinson <cohan@icnerd.com>
2017-09-16 20:13:50 +0100

    Change search request parameter to "query" instead of "q" as the search function was returning it as query which broke pagination

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,11 +11,11 @@ class SearchController extends Controller
     //
 
 	public function search (Request $request) {
-		if (empty($request->q)) {
+		if (!$request->has('query')) {
 			return redirect('/');
 		}
 
-		$videos = Video::search($request->q)->paginate(12);
+		$videos = Video::search($request->input('query'))->paginate(24);
 
 		return view('ydb.welcome')->with('videos', $videos);
 	}
