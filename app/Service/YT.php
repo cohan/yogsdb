23b1a341199c15797e1842e387437935d9c8d2f2
Cohan Robinson <cohan@icnerd.com>
2018-12-29 12:30:56 +0000

    Update the yt service blblbl

--- app/Service/YT.php
+++ app/Service/YT.php
@@ -16,99 +16,99 @@ use GuzzleHttp\Client;
 
 class YT {
 
-	public static function getVideo($id) {
-		$parts = [
-			'id',
-			'snippet',
-			'contentDetails',
-			'statistics',
-			'status',
-		];
+    public static function getVideo($id) {
+        $parts = [
+            'id',
+            'snippet',
+            'contentDetails',
+            'statistics',
+            'status',
+        ];
 
-		$youtubeVideo = Youtube::getVideoInfo($id, $parts) ?? false;
+        $youtubeVideo = Youtube::getVideoInfo($id, $parts) ?? false;
 
-		// TODO: Make this a better exception
-		if (!$youtubeVideo) {
-			if (Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
-				Video::where(['youtube_id' => $id])->delete();
-			}
+        // TODO: Make this a better exception
+        if (!$youtubeVideo) {
+            if (Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
+                Video::where(['youtube_id' => $id])->delete();
+            }
 
-			throw new \Exception("YouTube says that video doesn't exist. Removed from our DB.");
-		}
-		else { return $youtubeVideo; }
-	}
+            throw new \Exception("YouTube says that video doesn't exist. Removed from our DB.");
+        }
+        else { return $youtubeVideo; }
+    }
 
-	public static function addOrUpdateVideo($id, $latestOnly = null) {
-		$youtubeVideo = self::getVideo($id);
+    public static function addOrUpdateVideo($id, $latestOnly = null) {
+        $youtubeVideo = self::getVideo($id);
 
-		if ($latestOnly === true && Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
-			logger()->info("Update with latest flag for ".$id." but we already have it. Only updating tags.");
+        if ($latestOnly === true && Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
+            logger()->info("Update with latest flag for ".$id." but we already have it. Only updating tags.");
 
-			$video = Video::where(['youtube_id' => $id])->withTrashed()->first();
+            $video = Video::where(['youtube_id' => $id])->withTrashed()->first();
 
-			if ($video->trashed()) {
-				$video->restore();
-			}
+            if ($video->trashed()) {
+                $video->restore();
+            }
 
-			dispatch((new PatternTagStars($video))->onQueue('low'));
+            dispatch((new PatternTagStars($video))->onQueue('low'));
 
-			return;
-		}
+            return;
+        }
 
-		logger()->info("Updating or adding ".$id);
+        logger()->info("Updating or adding ".$id);
 
-		if (Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
-			$video = Video::where(['youtube_id' => $id])->withTrashed()->first();
-			if ($video->trashed()) {
-				$video->restore();
-			}
-		}
-		else {
-			$video = new Video();
-		}
+        if (Video::where(['youtube_id' => $id])->withTrashed()->exists()) {
+            $video = Video::where(['youtube_id' => $id])->withTrashed()->first();
+            if ($video->trashed()) {
+                $video->restore();
+            }
+        }
+        else {
+            $video = new Video();
+        }
 
-		$video->youtube_id = $youtubeVideo->id;
-		$video->title = $youtubeVideo->snippet->title;
-		$video->description = $youtubeVideo->snippet->description;
-		$video->upload_date = date("Y-m-d H:i:s", strtotime($youtubeVideo->snippet->publishedAt));
+        $video->youtube_id = $youtubeVideo->id;
+        $video->title = $youtubeVideo->snippet->title;
+        $video->description = $youtubeVideo->snippet->description;
+        $video->upload_date = date("Y-m-d H:i:s", strtotime($youtubeVideo->snippet->publishedAt));
 
-		// Stats
-		$video->duration = self::getDurationSeconds($youtubeVideo->contentDetails->duration) ?? 0;
+        // Stats
+        $video->duration = self::getDurationSeconds($youtubeVideo->contentDetails->duration) ?? 0;
 
-		if (empty($youtubeVideo->statistics->commentCount)) {
-			$video->comment_count = 0;
-		}
-		else {
-			$video->comment_count = $youtubeVideo->statistics->commentCount ?? 0;
-		}
+        if (empty($youtubeVideo->statistics->commentCount)) {
+            $video->comment_count = 0;
+        }
+        else {
+            $video->comment_count = $youtubeVideo->statistics->commentCount ?? 0;
+        }
 
-		if (empty($youtubeVideo->statistics->viewCount)) {
-			$video->view_count = 0;
-		}
-		else {
-			$video->view_count = $youtubeVideo->statistics->viewCount ?? 0;
-		}
+        if (empty($youtubeVideo->statistics->viewCount)) {
+            $video->view_count = 0;
+        }
+        else {
+            $video->view_count = $youtubeVideo->statistics->viewCount ?? 0;
+        }
 
-		if (empty($youtubeVideo->statistics->likeCount)) {
-			$video->like_count = 0;
-		}
-		else {
-			$video->like_count = $youtubeVideo->statistics->likeCount ?? 0;
-		}
+        if (empty($youtubeVideo->statistics->likeCount)) {
+            $video->like_count = 0;
+        }
+        else {
+            $video->like_count = $youtubeVideo->statistics->likeCount ?? 0;
+        }
 
-		if (empty($youtubeVideo->statistics->likeCount)) {
-			$video->dislike_count = 0;
-		}
-		else {
-			$video->dislike_count = $youtubeVideo->statistics->dislikeCount ?? 0;
-		}
+        if (empty($youtubeVideo->statistics->likeCount)) {
+            $video->dislike_count = 0;
+        }
+        else {
+            $video->dislike_count = $youtubeVideo->statistics->dislikeCount ?? 0;
+        }
 
-		if (empty($youtubeVideo->snippet->thumbnails->maxres->url)) {
-			$thumbnailUrl = $youtubeVideo->snippet->thumbnails->high->url;
-		}
-		else {
-			$thumbnailUrl = $youtubeVideo->snippet->thumbnails->maxres->url;
-		}
+        if (empty($youtubeVideo->snippet->thumbnails->maxres->url)) {
+            $thumbnailUrl = $youtubeVideo->snippet->thumbnails->high->url;
+        }
+        else {
+            $thumbnailUrl = $youtubeVideo->snippet->thumbnails->maxres->url;
+        }
 
         try {
             $youtubeThumbnail = file_get_contents($thumbnailUrl);
@@ -121,66 +121,66 @@ class YT {
         $video->thumbnail = Storage::url($video->youtube_id.'.jpg');
         Storage::setVisibility($video->youtube_id.'.jpg', 'public');
 
-		if (!empty($youtubeVideo->snippet->channelId)) {
-			$channel = Channel::where('youtube_id', '=', $youtubeVideo->snippet->channelId)->first();
+        if (!empty($youtubeVideo->snippet->channelId)) {
+            $channel = Channel::where('youtube_id', '=', $youtubeVideo->snippet->channelId)->first();
 
-			if (empty($channel) && !empty($youtubeVideo->snippet->channelTitle)) {
-				$channel = new Channel;
+            if (empty($channel) && !empty($youtubeVideo->snippet->channelTitle)) {
+                $channel = new Channel;
 
-				$channel->title = $youtubeVideo->snippet->channelTitle;
-				$channel->youtube_id = $youtubeVideo->snippet->channelId;
-				$channel->description = "";
-				$channel->slug = str_slug($channel->title);
-				$channel->save();
-			}
+                $channel->title = $youtubeVideo->snippet->channelTitle;
+                $channel->youtube_id = $youtubeVideo->snippet->channelId;
+                $channel->description = "";
+                $channel->slug = str_slug($channel->title);
+                $channel->save();
+            }
 
 
-			$video->channel_id = $channel->id;
-		}
+            $video->channel_id = $channel->id;
+        }
 
-		$video->slug = str_slug($video->title);
+        $video->slug = str_slug($video->title);
 
-		$video->updated_at = \Carbon\Carbon::now();
+        $video->updated_at = \Carbon\Carbon::now();
 
-		if (is_null($video->captions)) {
-			//$video->captions = self::getVideoTranscript($video->youtube_id);
-			//sleep(rand(1,5));
-		}
+        if (is_null($video->captions)) {
+            //$video->captions = self::getVideoTranscript($video->youtube_id);
+            //sleep(rand(1,5));
+        }
 
-		$video->save();
+        $video->save();
 
-		event(new VideoUpdated($video));
+        event(new VideoUpdated($video));
 
-		logger()->info("Task complete for ".$id);
+        logger()->info("Task complete for ".$id);
 
-	}
+    }
 
-	private static function getDurationSeconds($duration){
-		preg_match_all('/[0-9]+[HMS]/',$duration,$matches);
-		$duration=0;
-		foreach($matches as $match){
-	        //echo '<br> ========= <br>';       
-	        //print_r($match);      
-			foreach($match as $portion){        
-				$unite=substr($portion,strlen($portion)-1);
-				switch($unite){
-					case 'H':{  
-						$duration +=    substr($portion,0,strlen($portion)-1)*60*60;            
-					}break;             
-					case 'M':{                  
-						$duration +=substr($portion,0,strlen($portion)-1)*60;           
-					}break;             
-					case 'S':{                  
-						$duration +=    substr($portion,0,strlen($portion)-1);          
-					}break;
-				}
-			}
-	    //  echo '<br> duratrion : '.$duration;
-	    //echo '<br> ========= <br>';
-		}
-		return $duration;
+    private static function getDurationSeconds($duration){
+        preg_match_all('/[0-9]+[HMS]/',$duration,$matches);
+        $duration=0;
+        foreach($matches as $match){
+            //echo '<br> ========= <br>';       
+            //print_r($match);      
+            foreach($match as $portion){        
+                $unite=substr($portion,strlen($portion)-1);
+                switch($unite){
+                    case 'H':{  
+                        $duration +=    substr($portion,0,strlen($portion)-1)*60*60;            
+                    }break;             
+                    case 'M':{                  
+                        $duration +=substr($portion,0,strlen($portion)-1)*60;           
+                    }break;             
+                    case 'S':{                  
+                        $duration +=    substr($portion,0,strlen($portion)-1);          
+                    }break;
+                }
+            }
+        //  echo '<br> duratrion : '.$duration;
+        //echo '<br> ========= <br>';
+        }
+        return $duration;
 
-	}
+    }
 
     public static function getVideoGame($youtube_id) {
         $url = "https://gaming.youtube.com/watch?v=".$youtube_id;
@@ -202,40 +202,40 @@ class YT {
         return 'Unknown';
     }
 
-	public static function getVideoTranscript($id) {
-		$youtube_dl = env('YOUTUBE_DL');
+    public static function getVideoTranscript($id) {
+        $youtube_dl = env('YOUTUBE_DL');
 
-		$command = $youtube_dl.
-			" ".$id.
-			" --write-sub".
-			" --write-auto-sub".
-			" --skip-download".
-			" -o '".storage_path()."/captions/%(id)s'".
-			" | grep 'Writing video subtitles to:'";
+        $command = $youtube_dl.
+            " ".$id.
+            " --write-sub".
+            " --write-auto-sub".
+            " --skip-download".
+            " -o '".storage_path()."/captions/%(id)s'".
+            " | grep 'Writing video subtitles to:'";
 
-		exec($command, $output, $status);
+        exec($command, $output, $status);
 
-		if ($status !== 0) {
-			return false;
-		}
+        if ($status !== 0) {
+            return false;
+        }
 
-		$subtitles = explode(":", $output[0]);
-		$subtitlesPath = trim($subtitles[1]);
+        $subtitles = explode(":", $output[0]);
+        $subtitlesPath = trim($subtitles[1]);
 
-		$subtitles = Subtitles::load($subtitlesPath);
+        $subtitles = Subtitles::load($subtitlesPath);
 
-		// Keep things tidy
-		Storage::disk('local')->delete($subtitlesPath);
+        // Keep things tidy
+        Storage::disk('local')->delete($subtitlesPath);
 
-		$subtitles = $subtitles->getInternalFormat();
+        $subtitles = $subtitles->getInternalFormat();
 
-		foreach ($subtitles as $line) {
-			foreach ($line['lines'] as $caption) {
-				$captions[] = strip_tags($caption);
-			}
-		}
+        foreach ($subtitles as $line) {
+            foreach ($line['lines'] as $caption) {
+                $captions[] = strip_tags($caption);
+            }
+        }
 
-		return implode(" ",$captions);
-	}
+        return implode(" ",$captions);
+    }
 
 }
