af489ab7a3cd6ba33d89b80ba9e7707cbcb2b515
Cohan Robinson <cohan@icnerd.com>
2019-03-17 13:24:00 +0000

    Tweak Matomo code

--- resources/views/ydb/master.blade.php
+++ resources/views/ydb/master.blade.php
@@ -84,7 +84,6 @@
 	<script type="text/javascript">
 	  var _paq = window._paq || [];
 	  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-	  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 	  _paq.push(["setCookieDomain", "*.yogsdb.com"]);
 	  _paq.push(["setDomains", ["*.yogsdb.com"]]);
 	  _paq.push(['trackPageView']);
@@ -102,21 +101,6 @@
 
 </head>
 <body class="home">
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];
-a.async=1;
-a.src=g;
-m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-80857446-2', 'auto');
-		ga('send', 'pageview');
-
-	</script>
-
-
 	<nav class="navbar navbar-default navbar-static-top">
 		<div class="container">
 			<!-- Brand and toggle get grouped for better mobile display -->
