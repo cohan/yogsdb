cd694792cfa50f070f3b5387b7212ab68804b131
Cohan Robinson <cohan.robinson@wellstudio.co.uk>
2020-04-07 19:53:36 +0100

    Add note about busted API

--- resources/views/ydb/welcome.blade.php
+++ resources/views/ydb/welcome.blade.php
@@ -1,6 +1,12 @@
 @extends('ydb.master')
 
 @section('content')
+<div class="col-sm-9">
+    <div class="row">
+        <p>Aup. I know the videos aren't updating at the moment. YouTube want me to fill out a million forms and jump through a few thousand hoops to get the API access back. I intend on getting round to it, but my free time is a little stretched as it is right now</p>
+    </div>
+</div>
+
 <div class="col-sm-9 post_page_uploads">
 	<div class="row">
 
