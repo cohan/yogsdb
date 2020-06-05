adbbb6b40a32c36a27e7e68f729eb086d5dde50c
Cohan Robinson <cohan@icnerd.com>
2020-06-06 00:25:45 +0100

    Reset to latest Laravel

--- app/User.php
+++ app/User.php
@@ -2,18 +2,13 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use SoftDeletes;
-    use HasRoles;
-
 
     /**
      * The attributes that are mass assignable.
@@ -33,7 +28,12 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function alerts() {
-        return $this->hasMany('App\Alert');
-    }
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,58 +13,6 @@
 |
 */
 
-if (config('app.env') == 'production') {
-    $domain = "yogsdb.com";
-}
-else {
-    $domain = "";
-}
-
-if (config('app.env') == 'production') {
-	$apiDomain = "api.yogsdb.com";
-}
-else {
-	$apiDomain = "api.yogsdb.test";
-}
-
-Route::domain($domain)->group(function () {
-
-    Route::get('/schedule', function() {
-        return view('schedule.show');
-    });
-
-	Auth::routes();
-
-	Route::get('/home', function() {
-		return view('home');
-	});
-
-	Route::get('/', "HomeController@welcome");
-
-	Route::get('/search', "SearchController@search");
-
-	Route::get('/onthisday', "VideoController@onThisDay");
-
-	//Route::resource('series', 'SeriesController');
-
-	Route::resource('video', 'VideoController', [ 'only' => ['edit', 'update'] ]);
-
-	Route::resource('star', 'StarController', [ 'only' => ['edit', 'update'] ]);
-
-	Route::get('/game/{game}', "GameController@show");
-
-	Route::get('/star/{star}', "StarController@show");
-
-	Route::get('/tag/{tag}', function() { return "tbc"; } );
-
-	Route::get('/{channel}', "ChannelController@show");
-
-	Route::get('/{channel}/{video}', "VideoController@show");
-
-});
-
-Route::domain($apiDomain)->group(function () {
-	Route::get('/', function() {
-		return redirect("https://docs.yogsdb.com/api");
-	});	
+Route::get('/', function () {
+    return view('welcome');
 });
