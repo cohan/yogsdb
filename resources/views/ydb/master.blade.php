0923aea90a217f182c5f9fe916ab6c26f78bec5f
Cohan Robinson <cohan@icnerd.com>
2020-06-08 19:27:23 +0100

    Swap out an old or for a new PHP null coalesce

--- resources/views/ydb/master.blade.php
+++ resources/views/ydb/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>{{ $title or "Yogscast Database" }}</title>
+	<title>{{ $title ?? "Yogscast Database" }}</title>
 	@if (!empty($pageType) && $pageType == "video")
 
 	<!--
