9e0f7632047177ae976d1df3a9ada0c6ee7db267
Cohan Robinson <cohan@icnerd.com>
2020-06-26 07:44:37 +0100

    wip

--- app/Filters/VideoFilters.php
+++ app/Filters/VideoFilters.php
@@ -10,14 +10,6 @@ class VideoFilters extends QueryFilters {
 		return $this->builder->where("videos.youtube_id", "=", $youtube_id);
 	}
 
-	public function title($title) {
-        if (strtolower($title) == "ttt") {
-            return $this->builder->where("videos.title", "LIKE", "%GMOD TTT%")->inRandomOrder();
-        }
-
-		return $this->builder->where("videos.title", "LIKE", "%".$title."%");
-	}
-
 	public function description($description) {
 		return $this->builder->where("videos.description", "LIKE", "%".$description."%");
 	}
