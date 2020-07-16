0e4248bce0226538a7939ebfacec18f6b0bb7dc6
Cohan Robinson <cohan@icnerd.com>
2020-07-16 18:17:11 +0100

    wip

--- app/Scopes/UpdatedScope.php
+++ app/Scopes/UpdatedScope.php
@@ -17,8 +17,6 @@ class UpdatedScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
-        $builder->where('updated_at', '>=', now()->subDays(28))
-            ->where('title', '!=', '')
-            ->where('title', 'not null');
+        $builder->where('updated_at', '>=', now()->subDays(28));
     }
 }
--- resources/views/ydb/welcome.blade.php
+++ resources/views/ydb/welcome.blade.php
@@ -1,15 +1,6 @@
 @extends('ydb.master')
 
 @section('content')
-<div class="col-sm-9">
-    <div class="row">
-        <p>
-            Looks a bit bare here aye? I've had to delete all the data that hasn't been updated less than 28 days ago so that YouTube will re-enable the API key. Bare with!<br /><br />
-            As soon as it's back up I'll get everything updating in line with YT policies.
-        </p>
-    </div>
-</div>
-
 <div class="col-sm-9 post_page_uploads">
 	<div class="row">
 
