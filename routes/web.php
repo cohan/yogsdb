ca60b900a331bb374921d1066181447a1e82d997
Cohan Robinson <cohan@hey.com>
2020-11-08 00:30:41 +0000

    wip

--- routes/web.php
+++ routes/web.php
@@ -31,42 +31,9 @@ Route::domain($domain)->group(function () {
 		return redirect('https://schedule.yogs.app/');
     });
 
-	Auth::routes();
-
-	Route::get('/home', function() {
-		return view('home');
-	});
-
-	Route::get('/', function() {
-		return redirect('https://schedule.yogs.app/');
+	Route::fallback( function() {
+		return redirect('https://yogs.stream/');
     });
-
-    Route::get('/search', "SearchController@search");
-
-    Route::get('/terms', function () {
-        return view('policy.terms');
-    });
-    Route::get('/privacy', function () {
-        return view('policy.privacy');
-    });
-
-	Route::get('/onthisday', "VideoController@onThisDay");
-
-	//Route::resource('series', 'SeriesController');
-
-	Route::resource('video', 'VideoController', [ 'only' => ['edit', 'update'] ]);
-
-	Route::resource('star', 'StarController', [ 'only' => ['edit', 'update'] ]);
-
-	Route::get('/game/{game}', "GameController@show");
-
-	Route::get('/star/{star}', "StarController@show");
-
-	Route::get('/tag/{tag}', function() { return "tbc"; } );
-
-	Route::get('/{channel}', "ChannelController@show");
-
-	Route::get('/{channel}/{video}', "VideoController@show");
 
 });
 
